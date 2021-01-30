38699636a15908a38b63fe2b09ffff30b8a5c93c
AEONNIS <yogi333@yandex.ru>
2021-01-30 03:22:53 +0300

    Система инициализации игрока.

---
+++ Assets/InTheDark.Model/Player/IInitPlayerPresenter.cs
@@ -0,0 +1,9 @@
+﻿using InTheDark.Model.Components;
+
+namespace InTheDark.Model.Player
+{
+    public interface IInitPlayerPresenter
+    {
+        public void Init(in PlayerComponent player);
+    }
+}
--- Assets/InTheDark.Model/Systems/Presentation/PlayerInitSystem.cs
+++ Assets/InTheDark.Model/Systems/Presentation/PlayerInitSystem.cs
@@ -1,7 +1,25 @@
-﻿namespace InTheDark.Model.Systems
-{
-    public class PlayerInitSystem
-    {
+﻿using InTheDark.Model.Components;
+using InTheDark.Model.Player;
+using Leopotam.Ecs;
+using Leopotam.Ecs.Types;
 
+namespace InTheDark.Model.Systems
+{
+    public class PlayerInitSystem : IEcsInitSystem
+    {
+        private readonly EcsWorld _world = null;
+        private readonly IInitPlayerPresenter _initPlayerPresenter;
+
+        //To Config...
+        private readonly Int2 _initPlayerPosition = new Int2(15, 15);
+
+        public void Init()
+        {
+            var entity = _world.NewEntity();
+            ref var player = ref entity.Get<PlayerComponent>();
+            player = new PlayerComponent { Position = _initPlayerPosition };
+
+            _initPlayerPresenter.Init(player);
+        }
     }
 }
